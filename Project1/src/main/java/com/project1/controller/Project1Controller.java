0fb963e7503e97ca213ae294db6b86da86bb4ddc
PHANI <PHANI@PHANID-PC>
2015-12-10 17:17:52 +0530

    local update

--- Project1/src/main/java/com/project1/controller/Project1Controller.java
+++ Project1/src/main/java/com/project1/controller/Project1Controller.java
@@ -2,8 +2,8 @@ package com.project1.controller;
 
 import java.util.List;
 
-import com.project1.model.EmployeeModel;
-
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
@@ -14,18 +14,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.project1.model.DepartmentModel;
+import com.project1.model.EmployeeModel;
 import com.project1.service.ServiceBean;
+
 
 @Controller
 public class Project1Controller
-{   @Autowired
+{  
+	Logger log=LogManager.getLogger(Project1Controller.class);
+	@Autowired
 	ApplicationContext context;
   @RequestMapping(value="/get.form",method=RequestMethod.GET)
   public String get(@ModelAttribute("department") DepartmentModel dept,Model model)
   {
 	 model.addAttribute(dept); 
+	 log.info("Home page returned");
 	 return "home";
-	  
   }
   @RequestMapping(value="/get.form",method=RequestMethod.POST)
   public String get(ModelMap model,@ModelAttribute("department") DepartmentModel dept)
