307c1ccec21b25a2a60f6dc73f86b44232475a88
phani113 <phani401@gmail.com>
2015-12-10 23:35:21 +0530

    update daoimpl

--- Project1/src/main/java/com/project1/dao/Project1DAOImpl.java
+++ Project1/src/main/java/com/project1/dao/Project1DAOImpl.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-phani D
 import com.project1.domain.Employee;
 
 public class Project1DAOImpl implements Project1DAO {
